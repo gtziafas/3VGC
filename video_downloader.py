4cc0b9be1bd0a9e6151c179ee02befeb88004158
AndreiMiculita <andreimiculita@gmail.com>
2019-11-20 18:34:33 +0100

    Removed unneeded argument, added subtitles option

--- video_downloader.py
+++ video_downloader.py
@@ -4,7 +4,7 @@ import youtube_dl
 """Download a video in n second fragments"""
 
 
-def download_video(url: str, class_string: str, n: int):
+def download_video(url: str, class_string: str):
     # Create output dir
     output_directory = "out/" + class_string
     if not os.path.exists(output_directory):
@@ -12,15 +12,14 @@ def download_video(url: str, class_string: str, n: int):
 
     # Download the video in n second fragments
     for fragment in range(0, 2):
-        start_time = fragment * n
-
         ydl_opts = {
-            'outtmpl': output_directory + '/%(id)' + str(n) + 's%(ext)s',
-            'writesubtitles': True
+            'outtmpl': output_directory + '/%(id)s%(ext)s',
+            'writesubtitles': True,
+            'subtitlesformat': 'srt'
         }
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
     # Sample
 
 
-download_video('https://www.youtube.com/watch?v=QjL7D33xpS4', "drama", 5)
+download_video('https://www.youtube.com/watch?v=h2uDE8TuULs', "drama")
