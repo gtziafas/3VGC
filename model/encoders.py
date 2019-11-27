90f0203565f327ebb085f57b6340983ac8362190
gtziafas <pullmypill@hotmail.com>
2019-11-27 14:29:08 +0200

    textCNN

--- model/encoders.py
+++ model/encoders.py
@@ -11,9 +11,12 @@ class CNN3D(Module):
 
 # extract features from text with 1D convolutions ??
 class CNN1D(Module):
-	def __init__(self, *args, **kwargs) -> None:
+	def __init__(self, activation_fn: tensor_map, *args, **kwargs) -> None:
 		super(CNN1D, self).__init__()
-		NotImplemented
+		self.activation_fn = activation_fn
+		self.conv1 = Conv1d(in_channels=1,   out_channels=50,  kernel_size=3, stride=1)
+		self.conv2 = Conv1d(in_channels=1,   out_channels=50,  kernel_size=4, stride=1)
+		self.conv3 = Conv1d(in_channels=100, out_channels=100, kernel_size=2, stride=1)
 	def forward(self, x: FloatTensor) -> FloatTensor:
 		NotImplemented
 
@@ -23,7 +26,7 @@ class CNN1D(Module):
 
 # extract features from text with self-attention ??
 class TransformerEncoderLayer(Module):
-	def __init__(self, vector_size: int, num_heads: int=1, d_ff: float=2048, dropout_rate: float=0.1) -> None:
+	def __init__(self, vector_size: int, num_heads: int=6, d_ff: float=2048, dropout_rate: float=0.1) -> None:
 		super(TransformerEncoderLayer, self).__init__()
 		self.position_wise = PositionWiseFeedForward(d_model=vector_size, d_ff=d_ff, activation_fn=F.gelu)
 		self.mha = MultiHeadAttention(num_heads=num_heads, d_k=d_model, d_model=d_model, d_v=d_model)
@@ -46,7 +49,7 @@ class TransformerEncoderLayer(Module):
         return transformed_norm, mask
 
 class TransformerEncoder(Module):
-    def __init__(self, module_maker: Module, num_layers: int, *args, **kwargs) -> None:
+    def __init__(self, module_maker: Module, num_layers: int=1, *args, **kwargs) -> None:
         super(Encoder, self).__init__()
         self.positional_encoding = PositionalEncoding()
         self.network = Sequential(*[module_maker(*args, **kwargs) for _ in range(num_layers)])
@@ -70,4 +73,23 @@ class ModalityEncoder(Module):
 		return out 
 
 
+def example():
+	d_v, d_a, d_t = 10, 10, 10 
+	
+	# text encoding
+	seq_len = 5
+	vector_size = 512
+	attn_enc = TransformerEncoder(module_maker=TransformerEncoderLayer(),
+								 num_layers=1, vector_size=vector_size, num_heads=1)
+	text_encoder = ModalityEncoder(features=attn_enc, d_in=seq_len*vector_size, d_out=d_a)
 
+	# video encoding
+	h, w = 100, 100 
+	fv = 15 #15 frames per video sample
+	cnn3d = CNN3D()
+	video_encoder = ModalityEncoder(features=cnn3d, d_in=1024, d_out=d_v)
+
+	# audio encoding
+	fa = 150 #150 audio samples per video sample
+	# openSMILE 6392 feature vectors somehow
+	audio_encoder = ModalityEncoder(features=None, d_in=6392, d_out=d_a)
--- utils/imports.py
+++ utils/imports.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn 
 import torch.nn.functional as F
 from torch import FloatTensor, LongTensor 
-from torch.nn import Module, ModuleList, Linear, Sequential, RNN, LSTM, GRU, LayerNorm
+from torch.nn import Module, ModuleList, Linear, Sequential, RNN, LSTM, GRU, LayerNorm, Conv1d, Conv3d
 
 from typing import List, Tuple, Optional, Callable, Any, Sequence, TypeVar
 
