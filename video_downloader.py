0abfc5c807c82770a3960acee932d47a8bf55827
AndreiMiculita <andreimiculita@gmail.com>
2019-12-10 17:02:59 +0100

    renamed sample length argument

--- video_downloader.py
+++ video_downloader.py
@@ -5,13 +5,13 @@ import subprocess
 """Download a video in n second fragments"""
 
 
-def getLength(input_video):
+def get_length(input_video):
     result = subprocess.check_output(['ffprobe', '-i', input_video, '-show_entries', 'format=duration', '-v', 'quiet',
                                       '-of', 'csv=%s' % ("p=0")])
     return result.decode("utf-8")
 
 
-def download_video(url: str, class_string: str, n: int):
+def download_video(url: str, class_string: str, n_seconds: int = 30):
     # Create output dir
     output_directory = "out/" + class_string
     if not os.path.exists(output_directory):
@@ -31,16 +31,18 @@ def download_video(url: str, class_string: str, n: int):
         print(index)
 
         if filename.endswith(".mp4") or filename.endswith(".mkv"):
-            length = int(float(getLength(output_directory + '/' + filename)))
-            print(length)
+            length = int(float(get_length(output_directory + '/' + filename)))
+            print(length, int(length / n_seconds) - 1)
             # TODO: divide duration by n to get range size
-            for fragment in range(0, int(length/n) - 1):
-                start_time = str(fragment * n) + ":00"
+            for fragment in range(0, int(length / n_seconds) - 1):
+                start_seconds = fragment * n_seconds
+                start_time = str(int(start_seconds/60)) + ":" + str(start_seconds % 60)
+                print(start_time)
                 os.system('ffmpeg -ss ' + start_time + ' -i "' +
                           output_directory + '/' + filename +
                           '" -ss ' + start_time +
                           ' -i "' + output_directory + '/' + filename +
-                          '" -t 0:' + str(n) + ' -map 0:v -map 1:a -c:v libx264 -c:a aac ' +\
+                          '" -t 0:' + str(n_seconds) + ' -map 0:v -map 1:a -c:v libx264 -c:a aac ' + \
                           output_directory + '/' + os.path.splitext(filename)[0] + str(fragment) + '.mkv')
 
         if os.path.exists(output_directory + '/' + filename):
