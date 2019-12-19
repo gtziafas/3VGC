4ad1fed3177173da680368305fa2790526050f8c
AndreiMiculita <andreimiculita@gmail.com>
2019-12-19 17:46:13 +0100

    Separated subtitle splitting, added deletion of current output contents

--- video_downloader.py
+++ video_downloader.py
@@ -1,5 +1,8 @@
 import os
+import shutil
+
 import youtube_dl
+# from vtt_to_srt import vtt_to_srt
 import subprocess
 
 """Download a video in n second fragments"""
@@ -8,39 +11,53 @@ import subprocess
 
 def get_length(input_video):
     result = subprocess.check_output(['ffprobe', '-i', input_video, '-show_entries', 'format=duration', '-v', 'quiet',
-                                      '-of', 'csv=%s' % ("p=0")])
+                                      '-of', 'csv=%s' % "p=0"])
     return result.decode("utf-8")
 
 
-def download_video(url: str, class_string: str, n_seconds: int = 30):
+def download_videos(url: str, class_string: str, n_seconds: int = 30, split_videos=True, split_subs=True):
     # Create output dir
     output_directory = "out/" + class_string
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
+    else:
+        confirmation = 'a'
+        while confirmation != 'y' and confirmation != 'n':
+            confirmation = input("Delete current contents of output folder? (y/n): ")
+        if confirmation == 'y':
+            for filename in os.listdir(output_directory):
+                filepath = os.path.join(output_directory, filename)
+                try:
+                    shutil.rmtree(filepath)
+                except OSError:
+                    os.remove(filepath)
 
-    # Download the video in n second fragments
+    # Download the full video with subtitles
     ydl_opts = {
         'outtmpl': output_directory + '/%(id)s%(ext)s',
         'writesubtitles': True
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
-    # Sample
-    # For each downloaded file
-    for index, filename in enumerate(os.listdir(output_directory)):
-        print(index)
 
-        # Split files
-        if filename.endswith(".mp4") or filename.endswith(".mkv"):
+    # Lists of current downloaded files
+    file_list = os.listdir(output_directory)
+    video_list = list(filter(lambda x: x.endswith(".mp4") or x.endswith(".mkv"), file_list))
+    subs_list = list(filter(lambda x: x.endswith(".vtt"), file_list))
 
+    # Split videos
+    if split_videos:
+        for index, filename in enumerate(video_list):
+            # Get video length
             length = int(float(get_length(output_directory + '/' + filename)))
+            chunks = int(length / n_seconds) - 1
 
-            print(length, int(length / n_seconds) - 1)
+            print("video ", index, " has length ", length, "and", chunks, " chunks")
 
             # Divide duration by n to get range size
-            for fragment in range(0, int(length / n_seconds) - 1):
+            for fragment in range(0, chunks):
                 start_seconds = fragment * n_seconds
-                start_time = str(int(start_seconds/60)) + ":" + str(start_seconds % 60)
+                start_time = str(int(start_seconds / 60)) + ":" + str(start_seconds % 60)
                 print(start_time)
                 os.system('ffmpeg -ss ' + start_time + ' -i "' +
                           output_directory + '/' + filename +
@@ -53,11 +70,11 @@ def download_video(url: str, class_string: str, n_seconds: int = 30):
             if os.path.exists(output_directory + '/' + filename):
                 os.remove(output_directory + '/' + filename)
 
-        # Split subtitle files
-        elif filename.endswith(".vtt"):
-            #TODO
-            pass
+    # Split subtitles
+    if split_subs:
+        # vtt_to_srt(output_directory, rec=True)
+        pass
 
 
 if __name__ == "__main__":
-    download_video('https://www.youtube.com/watch?v=h2uDE8TuULs', "drama", 30)
+    download_videos('https://www.youtube.com/watch?v=h2uDE8TuULs', "drama", 30, split_videos=False)
