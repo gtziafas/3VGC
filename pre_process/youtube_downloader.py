ee3a97ac1ec50fd2c1c028ba6c7507d26946c716
gtziafas <pullmypill@hotmail.com>
2020-01-17 23:25:54 +0200

    pere_stuff

--- pre_process/youtube_downloader.py
+++ pre_process/youtube_downloader.py
@@ -36,23 +36,24 @@ def split_video(file: str, dur: int, write_name: str, out_format: str) -> None:
 
     for i in range(chunks):
         start_time = i * dur 
-        os.system('ffmpeg -i ' + file + ' -ss ' + str(start_time) +' -t ' + str(dur) + ' ' + write_name + '_' + str(i) + '.' + out_format)
+        os.system('ffmpeg -strict experimental -i ' + file + ' -ss ' + str(start_time) +' -t ' + str(dur) + ' ' + write_name + '_' + str(i) + '.' + out_format)
 
 
-def split_entire_dir(data_dir: str, duration: float, in_format: str, out_format: str) -> None:
+def split_entire_dir(data_dir: str, duration: float, in_format: str = 'mp4', out_format: str = 'mp4') -> None:
     pathlist = Path(data_dir).glob('**/*.' + in_format)
 
     crumbs = data_dir.split("/")
     out_dir = "/".join([crumbs[0], crumbs[1]]) + "/video/"
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
+
     for path in pathlist:
         file = str(path)
-        print(file)
+        #print(file)
         # break
         crumbs = file.split("/")
-        out_name = "/".join([crumbs[0], crumbs[1]]) + "/video/" + crumbs[-1].split(".")[0] + "_parsed"
-        print(out_name)
+        out_name = "/".join(crumbs[0:5]) + "/video/" + crumbs[-1].split(".")[0] + "_parsed"
+        #print(out_name)
         # break
         split_video(file, duration, out_name, out_format)
 
