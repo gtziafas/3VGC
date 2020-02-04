cd658151106b75c7e596398ec23275dc67e00d16
gtziafas <pullmypill@hotmail.com>
2020-02-04 18:10:23 +0200

    tfn_fixes

--- model/fusion.py
+++ model/fusion.py
@@ -24,4 +24,18 @@ class AverageFusion(Module):
     def forward(self, tensor_list: List[FloatTensor]) -> FloatTensor:
         return torch.mean(torch.stack(tensor_list), dim=0)
 
-# TODO: TensorFusion, HierarchicalFusion
+
+class TensorFusion(Module):
+    def __init__(self) -> None:
+        super(TensorFusion, self).__init__()
+
+    def forward(self, tensor_list: List[FloatTensor]) -> FloatTensor:
+        batch_size, vector_size = tensor_list[0].shape[0], tensor_list[0].shape[1]
+        vectors = [tensor.flatten() for tensor in tensor_list]
+
+        outter_2d = torch.ger(vectors[0], vectors[1])
+        outter_3d = torch.ger(outter_2d.flatten(), vectors[2]).view(batch_size, vector_size, vector_size, vector_size)
+
+        return outter_3d
+        
+# TODO: HierarchicalFusion
