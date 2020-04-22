142461c8f8705d57871faf436979ba6a7f956d13
Surender Harsha <battusurender96@gmail.com>
2020-04-22 22:59:20 +0200

    updated with batch loading and unloading
    To save memory

--- train_and_test.py
+++ train_and_test.py
@@ -55,32 +55,43 @@ if __name__ == "__main__":
         print('Using cuda.\n')
     else:
         print('Using cpu.\n')
-
-    # Run csv_extraction_script.py to get these files
-    train_csv_path = 'train_file_paths.csv'
-    test_csv_path = 'test_file_paths.csv'
-
-    # 16h of train set, 4h of test set
-    # Look in csv_extraction_script to estimate how much the maximum values are
-    train_set = AudioDataset(csv_path=train_csv_path, duration_per_category=16)
-    test_set = AudioDataset(csv_path=test_csv_path, duration_per_category=4)
-    print("Train set size: " + str(len(train_set)))
-    print("Test set size: " + str(len(test_set)))
-
-    kwargs = {'num_workers': 4,
-              'pin_memory': True} if device == 'cuda' else {}  # needed for using datasets on gpu
-
-    trainloader = torch.utils.data.DataLoader(train_set, batch_size=128, shuffle=True, **kwargs)
-    testloader = torch.utils.data.DataLoader(test_set, batch_size=128, shuffle=True, **kwargs)
-
     model = threevgc()
     model.to(device)
     optimizer = optim.Adam(model.parameters(), lr=0.01, weight_decay=0.0001)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
-    log_interval = 20
-    for epoch in range(1, 41):
-        if epoch == 21:
-            print("First round of training complete. Setting learn rate to 0.001.")
-        train(model, epoch)
-        test(model, epoch)
-        scheduler.step()
+    
+    
+    n_fold = 10
+    # Run csv_extraction_script.py to get these files
+    train_ = ['train'+str(i)+'.csv' for i in range(n_fold)]
+    test_ = ['test'+str(i)+'.csv' for i in range(n_fold)]
+    
+    
+    for i in range(n_fold):
+    # 16h of train set, 4h of test set
+        # Look in csv_extraction_script to estimate how much the maximum values are
+        train_csv_path = train_[i]
+        test_csv_path = test_[i]
+        train_set = AudioDataset(csv_path=train_csv_path, duration_per_category=16)
+        test_set = AudioDataset(csv_path=test_csv_path, duration_per_category=4)
+        print("Train set size: " + str(len(train_set)))
+        print("Test set size: " + str(len(test_set)))
+
+        kwargs = {'num_workers': 4,
+                  'pin_memory': True} if device == 'cuda' else {}  # needed for using datasets on gpu
+
+        trainloader = torch.utils.data.DataLoader(train_set, batch_size=128, shuffle=True, **kwargs)
+        testloader = torch.utils.data.DataLoader(test_set, batch_size=128, shuffle=True, **kwargs)
+
+
+        log_interval = 20
+        for epoch in range(1, 41):
+            if epoch == 21:
+                print("First round of training complete. Setting learn rate to 0.001.")
+            train(model, epoch)
+            test(model, epoch)
+            scheduler.step()
+        del trainloader
+        del testloader
+        del train_set
+        del test_set
